e4eb1e5b489fe26c0a994c3f38daed8725d44d22
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-12-23 13:44:58 -0500

    added sqlite3

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
@@ -19,11 +21,32 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
 func createEvent(context *gin.Context) {
 	var event models.Event
-	context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created", "event": event})
 }
